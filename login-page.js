f7d7b9ae33dcb160533cfabf876932003dedf1bd
mike <mike@pini.co.uk>
2020-05-10 11:10:59 +0100

    set links to point at home page not index

--- login-page.js
+++ login-page.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
-        window.location.href = "index.html";
+        window.location.href = "Home.html";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
